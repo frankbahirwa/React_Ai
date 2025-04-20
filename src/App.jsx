67f7cf63f5dba82307f834c6eebe147ba8fa14a5
frankbahirwa <frankbahirwa@gmail.com>
2025-04-20 17:27:37 +0200

    added url

--- src/App.jsx
+++ src/App.jsx
@@ -119,7 +119,7 @@ function App() {
         stress_level: parseFloat(stressLevel),
       };
 
-      const response = await axios.post('https://aquatic-brash-starburst.glitch.me/predict/'||'http://localhost:3001/predict', body);
+      const response = await axios.post('https://node-js-as-ai-proxy.vercel.app/predict', body);
       setResult(response.data);
     } catch (error) {
       console.error('Prediction error:', error);
